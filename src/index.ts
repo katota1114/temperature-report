748b869658ea6bbe3267b0c1513180ef4212a2b4
katota1114 <katota1114@gmail.com>
2021-12-11 00:16:21 +0900

    [fix] HTTPS通信に関わる部分を一旦コメントアウト

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,11 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const options : server.ServerOptions = {
-  key: fs.readFileSync("./privatekey.pem"),
-  cert: fs.readFileSync("./cert.pem"),
-};
-server.createServer(options, app).listen(8443);
+// const options : server.ServerOptions = {
+//   key: fs.readFileSync("./privatekey.pem"),
+//   cert: fs.readFileSync("./cert.pem"),
+// };
+// server.createServer(options, app).listen(8443);
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
