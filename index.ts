38d3570d71286dc5e5705d43f226e66a4a4bcaeb
katota1114 <katota1114@gmail.com>
2021-12-10 23:44:43 +0900

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import server from "https";
+import fs from "fs";
+
+const port = 3000;
+
+const app: express.Express = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const options : server.ServerOptions = {
+  key: fs.readFileSync("./privatekey.pem"),
+  cert: fs.readFileSync("./cert.pem"),
+};
+server.createServer(options, app).listen(8443);
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Start on port ${port}`);
+});
